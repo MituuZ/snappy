dd03b6789288b2d46d7a33b4d8445254a842fc75
Mitja Leino <mitja.leino@hotmail.com>
2024-08-10 01:18:39 -0400

    Add substring util function

--- src/util.rs
+++ src/util.rs
@@ -15,13 +15,36 @@ pub fn substring_with_padding(source: &String, start: usize, end: usize) -> Stri
     return result.iter().cloned().collect::<String>();
 }
 
+/// Get the substring from a &String
+/// Start is inclusive
+/// End is exclusive
+pub fn substring(source: &String, start: usize, end: usize) -> String {
+    let chars: Vec<char> = source.chars().collect();
+    let mut result: Vec<char> = vec![];
+    let mut i = start;
+    while i < chars.len() && i < end {
+        result.push(chars[i]);
+        i += 1;
+    }
+    return result.iter().cloned().collect::<String>();
+}
+
 pub enum CodingType {
     ENCODE,
     DECODE,
 }
 
 #[test]
-fn base_substring() {
+fn substring_tests() {
+    assert_eq!("asd", substring(&"asdf".to_string(), 0, 3));
+    assert_eq!("asd", substring(&"asd".to_string(), 0, 3));
+    assert_eq!("as", substring(&"asd".to_string(), 0, 2));
+    assert_eq!("asd", substring(&"asd".to_string(), 0, 5));
+    assert_eq!("", substring(&"asd".to_string(), 0, 0));
+}
+
+#[test]
+fn substring_with_padding_tests() {
     assert_eq!("asd", substring_with_padding(&"asdf".to_string(), 0, 3));
     assert_eq!("asd", substring_with_padding(&"asd".to_string(), 0, 3));
     assert_eq!("as", substring_with_padding(&"asd".to_string(), 0, 2));
